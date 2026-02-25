478d01f6a567a3042e59aaa8d0c8a85949914f53
jollyroger182 <hello@jollyy.dev>
2026-02-26 00:19:44 +0800

    add manifest and command

--- index.ts
+++ index.ts
@@ -2,6 +2,10 @@ import app from './src/slack'
 
 console.log('dolly is starting...')
 
+app.command(/^\/.*dolly$/, async ({ ack }) => {
+  await ack('hello world')
+})
+
 await app.start()
 
 console.log('dolly has started!')
